66ac51ae062ac2518995f12e8799c30890b92104
Hacker <oncwnuIR6uRc7BrVSLgJqkqxEdKk@git.weixin.qq.com>
2026-04-15 00:31:24 +0800

    init: H5 app packaged with Capacitor for Android
    - 影视搜索 H5 应用
    - Capacitor 打包 Android APK
    - GitHub Actions 自动构建
    - 支持 HTTP 明文请求
    - 离线可加载 UI

---
+++ android/app/src/main/java/com/h5app/moviesearch/MainActivity.java
@@ -0,0 +1,5 @@
+package com.h5app.moviesearch;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ update-assets.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# 更新 H5 资源并同步到 Android 项目
+set -e
+
+BASE_URL="http://123.207.196.231:8080"
+DIR="$(cd "$(dirname "$0")" && pwd)/web-assets"
+
+echo ">>> 下载 H5 资源..."
+curl -s "$BASE_URL/" -o "$DIR/index.html"
+curl -s "$BASE_URL/vite.svg" -o "$DIR/vite.svg"
+
+# 从 index.html 提取 JS 和 CSS 文件名
+JS_FILE=$(grep -oE 'src="/assets/[^"]+' "$DIR/index.html" | sed 's/src="//')
+CSS_FILE=$(grep -oE 'href="/assets/[^"]+' "$DIR/index.html" | sed 's/href="//')
+
+[ -n "$JS_FILE" ] && curl -s "$BASE_URL$JS_FILE" -o "$DIR$JS_FILE"
+[ -n "$CSS_FILE" ] && curl -s "$BASE_URL$CSS_FILE" -o "$DIR$CSS_FILE"
+
+echo ">>> 资源下载完成"
+echo ">>> 同步到 Android 项目..."
+cd "$DIR/.." && npx cap sync android
+
+echo ">>> 完成! 现在可以在 Android Studio 中重新构建 APK"
